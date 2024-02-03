ebbd25399cdf9a5b16142f6680bd13eb44d1af94
Softkorner <softKorner.ai@gmail.com>
2024-02-03 11:57:29 +0530

    Practice on List Questions

---
+++ ListPractice/Class1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ListPractice
+{
+    public class Class1
+    {
+
+        public void List( List<int> list)                   //Adding the elements of list
+        {
+            int sum = 0;
+            foreach (var element in list)
+            {
+                sum += element;
+            }
+            Console.WriteLine(sum);
+        }
+
+
+    }
+
+    public class Class2                                        //Remove Duplicates From a List
+    {
+        public void List(List<int> list)
+        {
+            List<int> uniqueList = new List<int>();
+            foreach (var element in list)
+            {
+                if (!uniqueList.Contains(element))
+                {
+                    uniqueList.Add(element);
+                }
+            }
+            foreach(var element in uniqueList)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+
+    public class Class3                                           //Check if a list is sorted in ascending order
+    {
+        public void List(List<int> list)
+        {
+            bool isSorted = true;
+            for (int i = 0; i < (list.Count - 1); i++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    isSorted = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isSorted);
+        }
+    }
+
+
+    public class Class4
+    {
+        public void List(List<int> list)                                //Reverse a given list:
+        {
+            for (int i = 0, j = list.Count - 1; i < j; i++, j--)
+            {
+                int temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public class Class5                                // Merge two lists into a single sorted list
+    {
+        public void List(List<int> list1, List<int> list2)
+        {
+            List<int> mergedList = new List<int>(list1);
+            foreach (var element in list2)
+            {
+                int index = 0;
+                while (index < mergedList.Count && element > mergedList[index])
+                {
+                    index++;
+                }
+                mergedList.Insert(index, element);
+            }
+
+            foreach(var element in mergedList)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+
+    public class Class6
+    {
+        public void List(List<int> list)    //List Rotation
+        {
+            int positionsToRotate = 3; 
+            for (int i = 0; i < positionsToRotate; i++)
+            {
+                int lastElement = list[list.Count - 1];
+                for (int j = list.Count - 1; j > 0; j--)
+                {
+                    list[j] = list[j - 1];
+                }
+                list[0] = lastElement;
+            }
+
+            foreach( var element in list)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+
+    public class Class7                 //count occurenece
+    {
+        public void List(List<int> list)
+        {
+            Dictionary<int, int> occurrences = new Dictionary<int, int>();
+            foreach (var element in list)
+            {
+                if (occurrences.ContainsKey(element))
+                {
+                    occurrences[element]++;
+                }
+                else
+                {
+                    occurrences[element] = 1;
+                }
+            }
+
+        }
+    }
+}
+
+
+    
+
+
---
+++ ListPractice/MainList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPractice
+{
+    public class MainList
+    {
+        static void Main() {
+        List<int> list1 = [ 2, 1, 4, 3, 5, 6, 7, 6, 9, 0];
+        List<int> list2 = [11,3,234,54,64,24,98,67,98];
+        Class1 user1 = new Class1();
+        user1.List(list1);
+        Class2 user2 = new Class2();
+        user2.List(list1);
+        Class3 user3 = new Class3();
+        user3.List(list1);
+        Class4 user4 = new Class4();
+        user4.List(list1);
+        Class5 user5 = new Class5();
+        user5.List(list1,list2);
+        Class6 user6 = new Class6();
+        user6.List(list1);
+        Class7 user7 = new Class7();
+        user7.List(list1);
+
+
+
+        }
+
+    }
+}
