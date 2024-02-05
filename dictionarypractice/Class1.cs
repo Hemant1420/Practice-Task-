792dfc5fd416a43d24e9308cd6e1bd89a759d91b
Softkorner <softKorner.ai@gmail.com>
2024-02-05 08:16:25 +0530

    Dictionary Problems

---
+++ dictionarypractice/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionarypractice
+{
+    public class Class1
+    {
+        public void Dict(List<string> list)                         //occurence of each word in a list of strings using dictionary
+        {
+            Dictionary<char,int> dict = new Dictionary<char,int>();
+            for (int i = 0; i < list.Count; i++) 
+            {
+                for(int j = 0; j < list[i].Length; j++)
+                {
+                    if (dict.ContainsKey(list[i][j]))
+                    {
+                        dict[list[i][j]]++;
+                    }
+                    else
+                    {
+                        dict[list[i][j]] = 1;
+                    }
+                }
+               
+
+            }
+            foreach (var c in dict)
+            {
+                char key = c.Key;
+                int value = c.Value;
+                Console.WriteLine(key + " : " + value);
+
+            }
+
+
+
+        }
+    }
+
+
+    public class Class2()                           //Merging two dictionary
+    {
+        public void dict(Dictionary<string, int> dict1, Dictionary<char, int>dict2)
+        {
+            foreach(var c in dict2)
+            {
+                string key = Convert.ToString(c.Key);
+                int value = c.Value;
+                dict1[key] = value;
+
+            }
+
+            foreach(var c in dict1)
+            {
+                string key1 = c.Key;
+                int value1 = c.Value;
+                Console.WriteLine(key1 + " : " + value1);   
+            }
+        }
+    }
+
+    public class Class3
+    {
+        public void dict(Dictionary<string, int> dict1, Dictionary<string, int> dict2)
+        {
+            foreach( var c in dict1)
+            {
+                if (dict2.ContainsKey(c.Key))
+                {
+                    string Key = c.Key;
+                    int value = c.Value;
+                    string key2 = c.Key;
+                    int value2 = dict2[c.Key];
+
+                    Console.WriteLine($" {Key} : {value}, {key2} : {value2}");
+
+
+
+
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
+
---
+++ dictionarypractice/Class2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionarypractice
+{
+    public class Maintest
+    {
+        static void Main()
+        {
+            List<string> list = ["Rahul", "Rohan", "Sahil"];
+            Dictionary<string, int> dict1 = new Dictionary<string, int>();
+            {
+                dict1["Rahul"] = 2;
+                dict1["Alice"] = 3;
+                dict1["Amey"] = 4;
+                dict1["Rohit"] = 5;
+            }
+
+            Dictionary<string, int> dict2 = new Dictionary<string, int>();
+            {
+                dict2["Rahul"] = 6;
+                dict2["Rohit"] = 7;
+                dict2["Bob"] = 8;
+                dict2["Nobi"] = 9;
+            }
+
+            Dictionary<char, int> dict3 = new Dictionary<char, int>();
+            {
+                dict3['a'] = 6;
+                dict3['b'] = 7;
+                dict3['c'] = 8;
+                dict3['d'] = 9;
+            }
+
+
+
+            Class1 user = new Class1();
+            user.Dict(list);
+            
+            Class2 user2 = new Class2();
+            user2.dict(dict1,dict3);
+
+            Class3 user3 = new Class3();    
+            user3.dict(dict1 ,dict2);
+
+        }
+
+
+
+
+
+    }
+}
